5ca8acac7b933d4f7266f6a10ca74030306cd97a
MM <>
2025-07-26 10:37:29 +0100

    added guardrail back in

--- notification/send-notification.js
+++ notification/send-notification.js
@@ -139,12 +139,10 @@ async function main() {
     // Check if there's an update today
     const today = getTodayDate();
     
-    //if (today !== politicsData.update) {
-    //  console.error('No update found for today');
-    //  process.exit(1);
-    //}
-
-    console.log('Today:', today);
+    if (today !== politicsData.update) {
+      console.error('No update found for today');
+      process.exit(1);
+    }
 
     console.log('Update found for today, generating email...');
 
